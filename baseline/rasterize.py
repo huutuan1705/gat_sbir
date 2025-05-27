1ba6c2204cdb32bd1c4a3c0a805deffd86605fdd
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-05-27 10:17:16 +0700

    update baseline

--- baseline/rasterize.py
+++ baseline/rasterize.py
@@ -6,3 +6,72 @@ import pickle
 
 from bresenham import bresenham
 from PIL import Image
+
+def draw_image(vector_image, side=400):
+    raster_image = np.zeros((int(side), int(side)), dtype=np.float32)
+    initX, initY = int(vector_image[0, 0]), int(vector_image[0, 1])
+    pixel_length = 0
+
+    for i in range(0, len(vector_image)):
+        if i > 0:
+            if vector_image[i - 1, 2] == 1:
+                initX, initY = int(vector_image[i, 0]), int(vector_image[i, 1])
+
+        cordList = list(bresenham(initX, initY, int(vector_image[i, 0]), int(vector_image[i, 1])))
+        pixel_length += len(cordList)
+
+        for cord in cordList:
+            if (cord[0] > 0 and cord[1] > 0) and (cord[0] < side and cord[1] < side):
+                raster_image[cord[1], cord[0]] = 255.0
+        initX, initY = int(vector_image[i, 0]), int(vector_image[i, 1])
+
+    raster_image = scipy.ndimage.binary_dilation(raster_image) * 255.0
+    return raster_image
+
+def draw_image_test(vector_images, side=400, steps=26):
+    for vector_image in vector_images:
+        pixel_length = 0
+        sample_freq = list(np.round(np.linspace(0,  len(vector_image), steps)[1:]))
+        sample_len = []
+        raster_images = []
+        raster_image = np.zeros((int(side), int(side)), dtype=np.float32)
+        initX, initY = int(vector_image[0, 0]), int(vector_image[0, 1])
+        
+        for i in range(0, len(vector_image)):
+            if i > 0: 
+                if vector_image[i-1, 2] == 1:
+                    initX, initY = int(vector_image[i, 0]), int(vector_image[i, 1])
+
+            cordList = list(bresenham(initX, initY, int(vector_image[i,0]), int(vector_image[i,1])))
+            pixel_length += len(cordList)
+
+            for cord in cordList:
+                if 0 < cord[0] < side and 0 < cord[1] < side:
+                    raster_image[cord[1], cord[0]] = 255.0
+            initX , initY = int(vector_image[i,0]), int(vector_image[i,1])
+
+            if i in sample_freq:
+                raster_images.append(scipy.ndimage.binary_dilation(raster_image) * 255.0)
+                sample_len.append(pixel_length)
+
+        raster_images.append(scipy.ndimage.binary_dilation(raster_image) * 255.0)
+        sample_len.append(pixel_length)
+
+    return raster_images, sample_len
+
+def preprocess(sketch_points, side=400):
+    sketch_points = sketch_points.astype(np.float32)
+    sketch_points[:, :2] = sketch_points[:, :2] / np.array([256, 256])
+    sketch_points[:, :2] = sketch_points[:, :2] * side
+    sketch_points = np.round(sketch_points)
+    return sketch_points
+
+def rasterize_sketch(sketch_points):
+    sketch_points = preprocess(sketch_points)
+    raster_images = draw_image(sketch_points)
+    return raster_images
+
+def rasterize_sketch_test(sketch_points, steps=25):
+    sketch_points = preprocess(sketch_points)
+    raster_images, _ = draw_image_test([sketch_points], steps=steps+1)
+    return raster_images
