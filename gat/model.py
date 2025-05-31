b39cf20cc59f62dabda7fd774308753eec06cc6d
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-05-31 10:27:04 +0700

    add model and update requirements

---
+++ gat/model.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from gat.gat_module import GraphAttentionLayer
+
+class FGSBIR_GAT(nn.Module):
+    def __init__(self, visual_feature_dim: int, 
+                 gat_out_per_head_dim: int, n_gat_heads: int,
+                 final_embedding_dim: int, num_gat_layers: int = 1,
+                 dropout: float = 0.1, alpha_gat: float = 0.2):
+        super(FGSBIR_GAT, self).__init__()
+        
