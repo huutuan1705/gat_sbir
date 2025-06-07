cf2c76f0a43b4948ce9008f93d3c6dd7e3be0107
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-06-07 21:26:55 +0700

    update datasets

---
+++ image_process/test_annotation.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+annotations_file = "ChairV2_labels.csv"
+num_classes = 19
+idx = 1
+img_labels_df = pd.read_csv(annotations_file)
+labels = img_labels_df.iloc[idx, -num_classes:].values
+
+print(labels)
