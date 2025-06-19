90687fa02fb589e74343c3a576d5fda47630c7fc
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-06-19 08:41:09 +0700

    update train, model and loss

--- gat/train.py
+++ gat/train.py
@@ -19,31 +19,42 @@ def get_dataloader(args):
     
     return dataloader_train, dataloader_test
 
-def evaluate_model(model, dataloader_test, label_adj_matrix):
+def evaluate_model(model, test_loader, all_label_indices, label_adj_matrix):
     with torch.no_grad():
         model.eval()
+        sketch_array_tests = []
+        sketch_names = []
+        image_array_tests = torch.FloatTensor().to(device)
+        image_names = []
+        for _, data_batch in enumerate(tqdm(test_loader)):
+            sketch_features_all = torch.FloatTensor().to(device)
+            _, _, _, positive_feature, _, sketch_features = model(
+                data_batch,
+                all_label_indices,
+                label_adj_matrix
+            )
         
-def train_one_epoch(model, train_loader, optimizer, all_label_indices, label_adj_matrix, device,
+def train_one_epoch(model, train_loader, optimizer, all_label_indices, label_adj_matrix,
                     loss_weights_config):
     model.train()
     total_loss_epoch = 0
     all_individual_losses_epoch = {}
     
-    for batch_idx, data_batch in enumerate(tqdm(train_loader)):
+    for _, data_batch in enumerate(tqdm(train_loader)):
         optimizer.zero_grad()
         
         true_labels_multihot = data_batch['labels'].to(device)
         
-        search_embeddings, prediction_scores, gcn_features = model(
+        search_embeddings, prediction_scores, gcn_features, positive_feature, negative_feature, sketch_features = model(
             data_batch,
             all_label_indices,
             label_adj_matrix
         )
         
         loss, _ = compute_migg_loss(
-            positive_feature=data_batch['positive_image'].to(device),
-            negative_feature=data_batch['negative_image'].to(device),
-            sketch_feature=data_batch['sketch_images'].to(device),
+            positive_feature=positive_feature.to(device),
+            negative_feature=negative_feature.to(device),
+            sketch_feature=sketch_features.to(device),
             gcn_processed_label_features=gcn_features,
             prediction_scores=prediction_scores,
             true_labels_multihot=true_labels_multihot,
