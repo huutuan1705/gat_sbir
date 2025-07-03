0ee944adebf3b980e67a869535de76c370fa676a
tuan.nh <tuan.nh@tinasoft.com.vn>
2025-07-03 09:48:06 +0700

    update

--- gat/utils.py
+++ gat/utils.py
@@ -26,8 +26,8 @@ def get_label_adjacency_matrix(train_loader: DataLoader, num_classes: int, devic
     num_samples_processed = 0
 
     for batch_idx, data_batch in enumerate(train_loader):
-        _, multi_hot_labels, *_ = data_batch 
-        multi_hot_labels = multi_hot_labels.numpy()
+        *_, multi_hot_labels = data_batch 
+        multi_hot_labels = multi_hot_labels.cpu().numpy()
 
         # Efficiently compute co-occurrences for the batch
         # For each sample, find pairs of active labels
